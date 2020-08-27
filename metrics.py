696b9048903df0ecde5c669183b3daf1f934a968
Chris Chiu <chiu@endlessm.com>
2020-08-27 15:59:10 +0800

    metrics: divide the metrics script into collector and uploader

--- metrics.py
+++ metrics.py
@@ -6,160 +6,35 @@ import subprocess
 import time
 import shutil
 
+class EOSMetrics():
+    def __init__(self):
+        self.metrics_cache_dir = '/var/cache/metrics'
+        self.tracking_id_path = '/etc/metrics/tracking-id'
+        self.machine_id_path = '/etc/machine-id'
+        self.systemd_service = 'eos-metrics-event-recorder.service'
+        self.metrics_config_path = '/etc/metrics/eos-metrics-permissions.conf'
+        self.eos_version = float(0)
 
-class MVPCollector():
-    OFFLINE_METRICS_DIR = 'eos-metrics-data-1'
-    METRICS_CACHE_DIR = '/var/cache/metrics'
-    OFFLINE_METRICS_DEVICE_PATH = None
-    OFFLINE_METRICS_DATA_PATH = None
-    TRACKING_ID_PATH = '/etc/metrics/tracking-id'
-    MACHINE_ID_PATH = '/etc/machine-id'
-
-    def __init__(self, storage_path=None):
-        if storage_path is None:
-            ''' 
-            TODO: Use dialog window instead
-            '''
-            print('Please assign the metrics data storage path')
-        else:
-            self.OFFLINE_METRICS_DEVICE_PATH= storage_path
-
-    def verify_eos_version(self):
+    def	get_eos_version(self):
         f = open("/etc/os-release")
         for line in f.readlines():
             if line.startswith("VERSION="):
                 fdot = line.find('=')
                 ldot = line.rfind('.')
-                print(line[fdot+2:ldot])
                 major = line[fdot+2:ldot]
-                if float(major) < 3.9:
-                    print("Please update to latest Endless OS")
-                else:
-                    print("Good to start uploading")
+                self.eos_version = major
+                return float(major)
 
-    def check_available_space(self):
-        print('Not yet implemented')
+    '''
+    def get_upload_enable_config:
+    configParser
+    '''
 
-    def check_if_duplicate(self, path = None):
-        '''
-        Check if directory name (tracking id) is duplicate
-        '''
-        print('Not yet implemented')
+    '''
+    def get_environment_config:
+    configParser
+    '''
 
-    def create_folder_for_machine(self):
-        target_dir = os.path.join(self.OFFLINE_METRICS_DEVICE_PATH, self.OFFLINE_METRICS_DIR)
-        if os.path.exists(target_dir) is False:
-            os.mkdir(target_dir)
-
-        if os.path.exists(self.TRACKING_ID_PATH) is True:
-            f = open(self.TRACKING_ID_PATH)
-            self.OFFLINE_METRICS_DATA_PATH = os.path.join(target_dir, f.read(32))
-            if os.path.exists(self.OFFLINE_METRICS_DATA_PATH) is False:
-                os.mkdir(self.OFFLINE_METRICS_DATA_PATH)
-            else:
-                subprocess.check_output(['rm', '-rf', self.OFFLINE_METRICS_DATA_PATH])
-                os.mkdir(self.OFFLINE_METRICS_DATA_PATH)
-            f.close()
-        elif os.path.exists(self.MACHINE_ID_PATH) is True:
-            f = open(self.MACHINE_ID_PATH)
-            self.OFFLINE_METRICS_DATA_PATH = os.path.join(target_dir, f.read(32))
-            if os.path.exists(self.OFFLINE_METRICS_DATA_PATH) is False:
-                os.mkdir(self.OFFLINE_METRICS_DATA_PATH)
-            else:
-                subprocess.check_output(['rm', '-rf', self.OFFLINE_METRICS_DATA_PATH])
-                os.mkdir(self.OFFLINE_METRICS_DATA_PATH)
-            f.close()
-        else:
-            print('There is no metrics data available')
-
-    def copy_tracking_id(self):
-        if os.path.exists(self.TRACKING_ID_PATH) is True:
-            # The tracking id file has permission -rwxrwsr-x  which
-            # will raise PermissionError for shutil.copy, use copyfile
-            # instead
-            shutil.copyfile(self.TRACKING_ID_PATH, os.path.join(self.OFFLINE_METRICS_DATA_PATH, os.path.basename(self.TRACKING_ID_PATH)))
-        elif os.path.exists(self.MACHINE_ID_PATH) is True:
-            shutil.copy(self.MACHINE_ID_PATH, self.OFFLINE_METRICS_DATA_PATH)
-        else:
-            printf('No tracking ID found')
-
-    def copy_metrics_data(self):
-        files = ['boot_offset_metafile', 'network_send_file', 'variants.dat', 'variants.dat.metadata']
-        for file in files:
-            shutil.copyfile(os.path.join(self.METRICS_CACHE_DIR, file), os.path.join(self.OFFLINE_METRICS_DATA_PATH, os.path.basename(file)))
-
-    def check_disk_usage(self):
-        print('Constructing')
-
-    def copy_data(self):
-        self.create_folder_for_machine()
-        self.copy_tracking_id()
-        self.copy_metrics_data()
-
-
-class MVPUploader():
-    OFFLINE_METRICS_DIR = 'eos-metrics-data'
-    OFFLINE_TMP_METRICS_DIR = '/var/tmp/metrics'
-    OFFLINE_STORAGE_PATH = None
-
-    def __init__(self, storage_path=None):
-        if storage_path is None:
-            ''' 
-            TODO: Use dialog window instead
-            '''
-            print('Please give the metrics data storage path')
-        else:
-            self.OFFLINE_STORAGE_PATH = storage_path
-
-    def check_upload_result(self):
-        '''
-        Check if the upload successful or not
-        '''
-        print('Constructing')
-
-    def remove_offline_metrics(self):
-        '''
-        Remove the data after upload done
-        '''
-        print('Constructing')
-
-    def check_os_version(self):
-        '''
-        We need the MVP tool run on > eos 3.9 for the upload
-        '''
-        os_version = configparser.ConfigParser()
-        os_version.read('/etc/os-release')
-        print(os_version)
-
-    def copy_metrics_data_and_upload(self):
-        eos_metrics_data_dir=os.path.join(self.OFFLINE_STORAGE_PATH, self.OFFLINE_METRICS_DIR)
-        for target in os.listdir(eos_metrics_data_dir):
-            # TODO: only iterates for directories
-            print("Directory %s:" % target)
-            os.mkdir(self.OFFLINE_TMP_METRICS_DIR)
-            target_dir=os.path.join(eos_metrics_data_dir, target)
-            for file in os.listdir(os.path.join(eos_metrics_data_dir, target)):
-                shutil.copy(os.path.join(target_dir, file), self.OFFLINE_TMP_METRICS_DIR)
-            subprocess.Popen(['sudo', 'chown', '-R', 'metrics:metrics', self.OFFLINE_TMP_METRICS_DIR])
-            persistent_cache_dir_arg = '--persistent-cache-directory=' + self.OFFLINE_TMP_METRICS_DIR
-            tracking_id_path_arg = '--tracking-id-file-path=' + self.OFFLINE_TMP_METRICS_DIR
-            #self.daemon = subprocess.Popen(['sudo', '-u', 'metrics', '/lib/eos-event-recorder-daemon/eos-metrics-event-recorder', persistent_cache_dir_arg, tracking_id_path_arg])
-            self.daemon = subprocess.Popen(['sudo', '-u', 'metrics', '/lib/eos-event-recorder-daemon/eos-metrics-event-recorder', persistent_cache_dir_arg])
-            #self.check_os_version()
-
-            time.sleep(3)
-            try: 
-                subprocess.check_output(['/usr/bin/eos-upload-metrics'])
-            except subprocess.CalledProcessError as e:
-                raise RuntimeError ('Upload Fail')
-            #self.daemon.kill()
-            subprocess.check_output(['sudo', 'killall', '/lib/eos-event-recorder-daemon/eos-metrics-event-recorder'])
-
-            subprocess.check_output(['sudo', 'rm', '-rf', self.OFFLINE_TMP_METRICS_DIR])
-
-    def __del__(self):
-        '''
-        Remove temporary cache data
-        '''
-        subprocess.check_output(['sudo', 'rm', '-rf', self.OFFLINE_TMP_METRICS_DIR])
-
+    '''
+    def get_service_state:
+    '''
