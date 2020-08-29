b3978e5e3e2bcd21e75021ce5ac69be376a1013c
Chris Chiu <chiu@endlessm.com>
2020-08-29 18:33:18 +0800

    metrics: add configparser to get metrics configs

--- metrics.py
+++ metrics.py
@@ -12,8 +12,10 @@ class EOSMetrics():
         self.tracking_id_path = '/etc/metrics/tracking-id'
         self.machine_id_path = '/etc/machine-id'
         self.systemd_service = 'eos-metrics-event-recorder.service'
-        self.metrics_config_path = '/etc/metrics/eos-metrics-permissions.conf'
+        self.metrics_config_file = '/etc/metrics/eos-metrics-permissions.conf'
         self.eos_version = float(0)
+        self.config = configparser.ConfigParser()
+        self.config.read(self.metrics_config_file)
 
     def	get_eos_version(self):
         f = open("/etc/os-release")
@@ -25,15 +27,11 @@ class EOSMetrics():
                 self.eos_version = major
                 return float(major)
 
-    '''
-    def get_upload_enable_config:
-    configParser
-    '''
+    def get_upload_enable_config(self):
+        print(self.config.get("global", "uploading_enabled"))
 
-    '''
-    def get_environment_config:
-    configParser
-    '''
+    def get_environment_config(self):
+        print(self.config.get("global", "environment"))
 
     def get_service_state(self):
         status = os.system('systemctl is-active --quiet ' + self.systemd_service)
