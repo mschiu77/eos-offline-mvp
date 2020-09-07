f8f2fd0ac2f0088c705d4958b201781110ec5f2c
Chris Chiu <chiu@endlessm.com>
2020-09-07 13:48:42 +0800

    metrics: add method to check eos-metrics-event-recorder process

--- metrics.py
+++ metrics.py
@@ -5,6 +5,7 @@ import os
 import subprocess
 import time
 import shutil
+import re
 
 class EOSMetrics():
     def __init__(self):
@@ -13,6 +14,7 @@ class EOSMetrics():
         self.machine_id_path = '/etc/machine-id'
         self.systemd_service = 'eos-metrics-event-recorder.service'
         self.metrics_config_file = '/etc/metrics/eos-metrics-permissions.conf'
+        self.metrics_proc_name = 'eos-metrics-event-recorder'
         self.eos_version = float(0)
         self.config = configparser.ConfigParser()
         self.config.read(self.metrics_config_file)
@@ -32,6 +34,21 @@ class EOSMetrics():
 
     def get_environment_config(self):
         print(self.config.get("global", "environment"))
+
+    def metrics_proc_exists(self):
+        ps = subprocess.Popen("ps ax -o pid= -o args= ", shell=True, stdout=subprocess.PIPE)
+        ps_pid = ps.pid
+        output = ps.stdout.read()
+        ps.stdout.close()
+        ps.wait()
+
+        for line in str(output).split('\n'):
+            res = re.findall("(\d+) (.*)", line)
+            if res:
+                pid = int(res[0][0])
+                if self.metrics_proc_name in res[0][1] and pid != os.getpid() and pid != ps_pid:
+                    return True
+            return False
 
     def get_service_state(self):
         status = os.system('systemctl is-active --quiet ' + self.systemd_service)
